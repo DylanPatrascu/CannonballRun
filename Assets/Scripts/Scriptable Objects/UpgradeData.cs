f9ed5ca5e85584a6d4900e86c6616360c56ca6a1
Andrew Stephen Mohammed <amoha464@uwo.ca>
2025-03-17 16:52:33 -0400

    Added scrap economy

--- Assets/Scripts/Scriptable Objects/UpgradeData.cs
+++ Assets/Scripts/Scriptable Objects/UpgradeData.cs
@@ -13,6 +13,7 @@ public class UpgradeData : ScriptableObject
     public string upgradeName;
     public string description;
     public string flavour;
+    public int cost;
     public int value;
     public types type;
     public Texture2D icon;
